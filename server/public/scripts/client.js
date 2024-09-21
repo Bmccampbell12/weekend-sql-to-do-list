fa853310402c50d7546984e384dd1759969be162
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-09-21 01:54:10 -0500

    Updated data testid and buttons in index.html, updated css with styling for body and container, and wrote my starting function, submit event function, and axios function call.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,34 @@
 console.log('JS is sourced!');
+startSqlToDoList();
+
+//The 'Add' and 'Delete' buttons will go here. 
+
+function submitButton(event) {
+    event.preventDefault();
+
+    console.log('Submit To-Do task clicked');
+
+    let todos = {};
+    let toDoItem = document.getElementById('todos').value
+    addToDo(toDoItem);
+}
+
+function addToDo(toDoToAdd) {
+axios({
+    method: 'POST',
+    url:'/todos',
+    data: addToDo,
+}).then(function(response) {
+    console.log('addToDo()', response.data);
+    refreshToDo();
+}).catch(function(error) {
+    console.log('Error in POST', error)
+    alert('unable to add To-Do, please try again later.');
+});
+
+}
+
+
+
+
+
